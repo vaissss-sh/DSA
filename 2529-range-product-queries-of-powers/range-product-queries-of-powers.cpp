d7e70f1016cc334b25614e64d4e4377da8565c8f
Vaishnavi Shukla <157621773+vaissss-sh@users.noreply.github.com>
2025-08-11 11:11:52 +0530

    Time: 1 ms (97.87%) | Memory: 148 MB (39.89%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -8,16 +8,27 @@ public:
             }
         }
 
+        vector<int> pref(powers.size()+1);
+        for(int i=1;i<pref.size();i++){
+            pref[i]=pref[i-1]+powers[i-1];
+        }
+
         vector<int> ans;
         int mod=1e9+7;
+        vector<int> pmod(pref[pref.size()-1]+1,1);
+        for(int i=1;i<pmod.size();i++){
+            pmod[i]=(pmod[i-1]*2)%mod;
+        }
+
         for(int i=0;i<queries.size();i++){
             int l=queries[i][0];
             int r=queries[i][1];
-            long long multi=1;
-            for(int j=l;j<=r;j++){
-                multi=(multi* (1<< powers[j]))%mod;
-            }
-            ans.push_back(multi);
+            int sum=pref[r+1]-pref[l];
+            // long long multi=1;
+            // for(int j=l;j<=r;j++){
+            //     multi=(multi* (1<< powers[j]))%mod;
+            // }
+            ans.push_back(pmod[sum]);
         }
         return ans;
     }
