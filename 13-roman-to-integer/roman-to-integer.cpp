fda634ecf305f92d4aef83aa7f990b88909297d3
Vaishnavi Shukla <157621773+vaissss-sh@users.noreply.github.com>
2026-02-19 14:52:02 +0530

    Time: 5 ms (58.48%) | Memory: 13.6 MB (6.77%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int res=0;
-        unordered_map<char, int> roman = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-        };
+        map<char,int>m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
 
-        for(int i=0;i<s.size()-1;i++){
-            if(roman[s[i]] < roman[s[i+1]]){
-                res-=roman[s[i]];
+        int ans=0;
+        for(int i=0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                ans =ans-m[s[i]];
             }
             else{
-                res+=roman[s[i]];
+                ans +=m[s[i]];
             }
         }
-        return res+roman[s.back()];
+        return ans;
     }
 };
