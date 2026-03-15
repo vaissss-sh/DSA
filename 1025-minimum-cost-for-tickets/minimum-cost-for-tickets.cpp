d22df4ae166b0e751213b4d146dc9e2e57ea80a8
Vaishnavi Shukla <157621773+vaissss-sh@users.noreply.github.com>
2026-03-15 10:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (55.71%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+
+    //mem
+    int solve(int n,vector<int>& days, vector<int>& costs,int index,vector<int> &dp){
+       if(index >= n){
+           return 0;
+       }
+       if(dp[n]!=-1){
+           return dp[n];
+       }
+       //1 day pass
+       int option1 = costs[0]+solve(n,days,costs,index+1,dp);
+       int i;
+       // 7 day pass
+       for(i =index;i<n && days[i]<days[index]+7;i++);
+       int option2 = costs[1]+solve(n,days,costs,i,dp);
+
+       // 30 days pass 
+       for(i =index;i<n && days[i]<days[index]+30;i++);
+       int option3 = costs[2]+solve(n,days,costs,i,dp);
+        dp[index]=min(option1,min(option2,option3));
+       return dp[index];
+    }
+
+
+    int solve(int n,vector<int>& days, vector<int>& costs,int index){
+       if(index >= n){
+           return 0;
+       }
+
+
+       //1 day pass
+       int option1 = costs[0]+solve(n,days,costs,index+1);
+       int i;
+
+
+       // 7 day pass
+       for(i =index;i<n && days[i]<days[index]+7;i++);
+       int option2 = costs[1]+solve(n,days,costs,i);
+
+       // 30 days pass 
+       for(i =index;i<n && days[i]<days[index]+30;i++);
+       int option3 = costs[2]+solve(n,days,costs,i);
+
+
+       return min(option1,min(option2,option3));
+    }
+
+    int  solve(int n,vector<int>& days, vector<int>& costs){
+        vector<int> dp(n+1,INT_MAX);
+        dp[n] = 0;
+        for(int k=n-1;k>=0;k--){
+
+            //1 day pass
+            int option1 = costs[0]+dp[k+1];
+            int i;
+            // 7 day pass
+            for(i =k;i<n && days[i]<days[k]+7;i++);
+            int option2 = costs[1]+dp[i];
+
+            // 30 days pass 
+            for(i =k;i<n && days[i]<days[k]+30;i++);
+            int option3 = costs[2]+dp[i];
+                dp[k] = min(option1,min(option2,option3));
+        }
+        return dp[0];
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // int n = days.size();
+        // return solve(n,days,costs,0);
+
+        int n = days.size();
+        return solve(n,days,costs);
+    }
+};
