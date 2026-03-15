aafb42a1d28b6594e956810ee77bad2f56d45bee
Vaishnavi Shukla <157621773+vaissss-sh@users.noreply.github.com>
2026-03-15 09:39:26 +0530

    Time: 4 ms (29.42%) | Memory: 9.3 MB (46.81%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -34,8 +34,28 @@ public:
         dp[target]=sum;
         return dp[target];
     }
+
+    int solveTab(vector<int> &nums, int target){
+
+        vector<unsigned int> dp(target + 1, 0);
+
+        dp[0] = 1;
+
+        for(int i = 1; i <= target; i++){
+
+            for(int j = 0; j < nums.size(); j++){
+                if(i - nums[j] >= 0)
+                    dp[i] += dp[i - nums[j]];
+            }
+        }
+
+        return dp[target];
+
+    }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1);
-        return solvemem(nums,target,dp);
+        //vector<int>dp(target+1,-1);
+        //return solvemem(nums,target,dp);
+
+        return solveTab(nums,target);
     }
 };
