89ac648565be163fb9be08e7024a48debd8b8a4f
Vaishnavi Shukla <157621773+vaissss-sh@users.noreply.github.com>
2026-03-17 16:13:21 +0530

    Time: 7 ms (72.81%) | Memory: 30.7 MB (84.03%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -39,16 +39,44 @@ public:
             return dp[i][j] = 0;
         }
     }
+    int solveTab(vector<vector<char>>& matrix, int &maxi){
+        int n = matrix.size();
+        int m = matrix[0].size();
 
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i= n - 1;i >= 0;i--){
+            for(int j = m - 1; j >= 0; j--){
+                int diagonal = dp[i + 1][j + 1];
+                int right = dp[i][j + 1];
+                int down = dp[i + 1][j];
+
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(maxi, dp[i][j]);
+                    
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }  
+        return dp[0][0];  
+    }
     int maximalSquare(vector<vector<char>>& matrix) {
         // int maxi=0;
         // solveRec(matrix,0,0,maxi);
         // return maxi*maxi;
 
-        int n = matrix.size(), m = matrix[0].size();
+        // int n = matrix.size(), m = matrix[0].size();
+        // int maxi = 0;
+        // vector<vector<int>>dp(n, vector<int>(m, -1));
+        // solveMem(matrix, 0, 0, maxi, dp);
+        // return maxi*maxi;
+
         int maxi = 0;
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        solveMem(matrix, 0, 0, maxi, dp);
+        solveTab(matrix, maxi);
         return maxi*maxi;
+
     }
 };
