dc085217e2236d1f6f9921dedd8c5a634fdc9228
Vaishnavi Shukla <157621773+vaissss-sh@users.noreply.github.com>
2026-05-28 21:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (88.20%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -3,10 +3,12 @@ public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         int l=0;
         int h=letters.size()-1;
+        int ans=letters[0];
         while(l<=h){
             int mid=l+(h-l)/2;
 
             if(target<letters[mid]){
+                ans=letters[mid];
                 h=mid-1;
             }
             else{
@@ -14,6 +16,6 @@ public:
             }
         }
 
-        return letters[l%letters.size()];
+        return ans;
     }
 };
